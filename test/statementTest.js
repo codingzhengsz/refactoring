8ae6d38e1ee9b42235d6dd31d203c76e6fc044a9
Martin <codingzhengsz@163.com>
2020-08-27 16:44:16 +0800

    [Refactor][Martin] test: add htmlStatement case

--- test/statementTest.js
+++ test/statementTest.js
@@ -1,5 +1,5 @@
 const test = require('ava');
-const {statement} = require('../src/statement');
+const {statement, htmlStatement} = require('../src/statement');
 
 const plays = {
     'hamlet': {
@@ -15,7 +15,6 @@ const plays = {
         'type': 'tragedy',
     },
 };
-
 
 test('statement case 1, Customer Martin has 0 performance', t => {
     const invoice = {
@@ -139,4 +138,33 @@ test('statement case 7, Customer Martin has 2 performances and the audience is 1
         ' As You Like It: $345.00 (15 seats)\n' +
         'Amount owed is $745.00\n' +
         'You earned 3 credits \n')
+})
+
+test('htmlStatement case 1, Customer Martin has 3 performances and the audience is 35', t => {
+    const invoice = {
+        'customer': 'Martin',
+        'performances': [{
+            'playID': 'hamlet',
+            'audience': 55,
+        }, {
+            'playID': 'as-like',
+            'audience': 35,
+        }, {
+            'playID': 'othello',
+            'audience': 40,
+        }]
+    };
+
+    const result = htmlStatement(invoice, plays);
+
+    t.is(result, '<h1>Statement for Martin</h1>\n' +
+        '<table>\n' +
+        '<tr><th>play</th><th>seats</th><th>cost</th></tr>' +
+        ' <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n' +
+        ' <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n' +
+        ' <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n' +
+        '</table>\n' +
+        '<p>Amount owed is <em>$1,730.00</em></p>\n' +
+        '<p>You earned <em>47</em> credits</p>\n');
+
 })
